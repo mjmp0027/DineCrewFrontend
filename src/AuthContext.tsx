72e4a60437a4089ad4d346268dbc345e3a8c491d
mjmp0027 <mjmp0027@red.ujaen.es>
2024-08-01 11:32:36 +0200

    Agrega iconos para la mejora de la visualización y mejora con ayuda del contexto la autenticación

---
+++ src/AuthContext.tsx
@@ -0,0 +1,84 @@
+import React, {
+  createContext,
+  useState,
+  useEffect,
+  useContext,
+  ReactNode,
+} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import decodeJwtToken from './decodeJwtToken';
+
+interface AuthContextType {
+  user: any;
+  loading: boolean;
+  login: (token: string) => Promise<void>;
+  logout: () => Promise<void>;
+}
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider: React.FC<AuthProviderProps> = ({children}) => {
+  const [user, setUser] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
+
+  const checkToken = async () => {
+    const token = await AsyncStorage.getItem('token');
+    if (token) {
+      try {
+        const response = await fetch(
+          'http://10.0.2.2:8080/api/auth/validate-token',
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(token),
+          },
+        );
+        const data = await response.json();
+        if (data.isValid) {
+          setUser(decodeJwtToken(token));
+        } else {
+          await AsyncStorage.removeItem('token');
+          setUser(null);
+        }
+      } catch (error) {
+        console.error(error);
+        setUser(null);
+      }
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    checkToken();
+  }, []);
+
+  const login = async (token: string) => {
+    await AsyncStorage.setItem('token', token);
+    setUser(decodeJwtToken(token));
+  };
+
+  const logout = async () => {
+    await AsyncStorage.removeItem('token');
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{user, login, logout, loading}}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = (): AuthContextType => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+};
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,109 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Alert, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../types';
+import Icon from 'react-native-vector-icons/Ionicons';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import decodeJwtToken from '../decodeJwtToken';
+import {useAuth} from '../AuthContext';
 
-const HomeScreen: React.FC = () => {
+type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
+
+const HomeScreen: React.FC<Props> = ({navigation}) => {
+  const {logout} = useAuth();
+  const [role, setRole] = useState<string | null>(null);
+
+  useEffect(() => {
+    const getUserRole = async () => {
+      const token = await AsyncStorage.getItem('token');
+      if (token) {
+        const decodedToken = decodeJwtToken(token);
+        const username = decodedToken.sub;
+
+        const response = await fetch(
+          `http://10.0.2.2:8080/api/users/${username}`,
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${token}`,
+            },
+          },
+        );
+
+        if (response.ok) {
+          const userData = await response.json();
+          setRole(userData.role);
+        } else {
+          Alert.alert('Error', 'No se pudo obtener el rol del usuario.');
+        }
+      }
+    };
+
+    getUserRole();
+  }, []);
+  const handleLogout = async () => {
+    const token = await AsyncStorage.getItem('token');
+    if (token) {
+      const decodedToken = decodeJwtToken(token);
+      const username = decodedToken.sub;
+      const response = await fetch(
+        `http://10.0.2.2:8080/api/auth/logout/${username}`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+      if (response.ok) {
+        await logout();
+        Alert.alert('Cerrar sesión', 'Has cerrado sesión con éxito.');
+      } else {
+        Alert.alert('Error', 'Hubo un problema al cerrar sesión.');
+      }
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Bienvenido a la pantalla de inicio</Text>
+      <View style={styles.menu}>
+        <Text style={styles.menuText}>Menú</Text>
+        <View style={styles.icons}>
+          <TouchableOpacity
+            onPress={() =>
+              Alert.alert('Notificaciones', 'No tienes notificaciones nuevas.')
+            }>
+            <Icon name="notifications-outline" size={25} style={styles.icon} />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={handleLogout}>
+            <Icon name="person-circle-outline" size={25} style={styles.icon} />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={styles.content}>
+        {role === 'CAMARERO' && (
+          <>
+            <TouchableOpacity
+              style={styles.box}
+              onPress={() => navigation.navigate('Mesas')}>
+              <Text style={styles.boxText}>Mesas</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.box}
+              onPress={() => navigation.navigate('Comandas')}>
+              <Text style={styles.boxText}>Mis Comandas</Text>
+            </TouchableOpacity>
+          </>
+        )}
+        {role === 'COCINERO' && (
+          <TouchableOpacity
+            style={styles.box}
+            onPress={() => navigation.navigate('Comandas')}>
+            <Text style={styles.boxText}>Comandas</Text>
+          </TouchableOpacity>
+        )}
+      </View>
     </View>
   );
 };
@@ -12,8 +111,46 @@ const HomeScreen: React.FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#f5f5f5',
+  },
+  menu: {
+    height: 60,
+    backgroundColor: '#6200EE',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+  },
+  menuText: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  icons: {
+    flexDirection: 'row',
+  },
+  icon: {
+    marginLeft: 15,
+    color: 'white',
+  },
+  content: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  box: {
+    width: 150,
+    height: 150,
+    backgroundColor: '#6200EE',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 20,
+    borderRadius: 10,
+  },
+  boxText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
 
